6e3b35c540cd57ff7ebcf9825aa9aeba929c708c
Jaffar <jacepac264@gmail.com>
2022-08-04 16:01:55 +0300

    added background

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -4,10 +4,10 @@ const ErrorModal = (props) => {
   return (
     <div className="fixed inset-0 bg-zinc-300/5 z-10 backdrop-blur-sm flex transition duration-500">
       <div className="m-auto w-2/3 md:w-2/5 backdropdrop-shadow-lg">
-        <div className="bg-emerald-700 rounded-t-md text-2xl p-2 text-white font-medium">
+        <div className="bg-gradient-to-l from-emerald-700 to-slate-900 rounded-t-md text-2xl p-2 text-white font-medium">
           {props.title}
         </div>
-        <div className="bg-zinc-300/10 backdrop-blur-sm rounded-b-md">
+        <div className="bg-zinc-300/60 backdrop-blur-sm rounded-b-md">
           <div className="text-xl p-6">{props.details}</div>
           <div className="flex">
             <Button className="px-8 ml-auto m-2" onClick={props.onConfirm}>Okay</Button>
--- src/components/UI/FullWrapper.js
+++ src/components/UI/FullWrapper.js
@@ -1,5 +1,7 @@
+import styles from "./FullWrapper.module.css"
+
 const FullWrapper = props => {
-    return <div className="h-full overflow-scroll bg-gradient-to-br from-emerald-700 to-slate-900">
+    return <div className={`h-full overflow-scroll bg-gradient-to-br from-emerald-700 to-slate-900 ${styles.bg}`}>
         {props.children}
     </div>
 }
