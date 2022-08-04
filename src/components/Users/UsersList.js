7db5b3463005b3c1a205c7492f55663284257de3
Jaffar <jacepac264@gmail.com>
2022-08-04 16:37:55 +0300

    removed container while empty

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -2,12 +2,12 @@ import TransparentContainer from "../UI/TransparentContainer";
 import UserItem from "./UserItem";
 
 const UsersList = (props) => {
-  return (
-    <TransparentContainer className="mt-5 flex flex-col gap-3 max-h-64 overflow-y-scroll scroll-py-2 snap-y">
-      {props.users.map((user) => 
-        <UserItem username={user.username} age={user.age} id={user.id} />
-      )}
-    </TransparentContainer>
+  return ( props.users.length > 0 && 
+      <TransparentContainer className="mt-5 flex flex-col gap-3 max-h-64 overflow-y-scroll scroll-py-2 snap-y">
+        {props.users.map((user) => (
+          <UserItem username={user.username} age={user.age} id={user.id} />
+        ))}
+      </TransparentContainer>
   );
 };
 
