832f1c80def9c5da4c8b008210351708a4977269
Jaffar Totanji <jacepac264@gmail.com>
2023-03-19 23:38:33 +0300

    [fix] remove chrome scrollbar

--- src/components/UI/FullWrapper.js
+++ src/components/UI/FullWrapper.js
@@ -1,7 +1,7 @@
 import styles from "./FullWrapper.module.css"
 
 const FullWrapper = props => {
-    return <div className={`h-full overflow-scroll bg-gradient-to-br from-emerald-700 to-slate-900 ${styles.bg}`}>
+    return <div className={`h-full overflow-scroll bg-gradient-to-br from-emerald-700 to-slate-900 ${styles.bg} ${styles.noscroll}`}>
         {props.children}
     </div>
 }
